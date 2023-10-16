40da4175b8de08228093e510219925ca6abb6039
Furkan ADIKTI <furkan@Furkan-MBP.home>
2023-10-16 21:34:04 +0300

    implement portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
+import robotsTxt from "astro-robots-txt";
+import UnoCSS from "@unocss/astro";
+
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.0xy.dev/",
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: [
+        "https://0xy.dev/sitemap-index.xml",
+        "https://0xy.dev/sitemap-0.xml",
+      ],
+    }),
+    solidJs(),
+    UnoCSS({ injectReset: true }),
+  ],
+  output: "server",
+  adapter: netlify(),
+});
---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const LINKS = {
+  github: "https://github.com/0xydev",
+  linkedin: "https://www.linkedin.com/in/furkanadkt/",
+};
