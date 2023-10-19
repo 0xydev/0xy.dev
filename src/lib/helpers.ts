b775f5faf937f9264878991709777413f1f281c1
Furkan <71332099+0xydev@users.noreply.github.com>
2023-10-19 15:35:27 +0300

    update time zone and format

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -19,14 +19,12 @@ export function formatTimeForItaly(date: Date): string {
     minute: "2-digit",
     second: "2-digit",
     hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Turkey",
+    timeZone: "Europe/Istanbul",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
 
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
+  formattedTime += " TRT";
 
   return formattedTime;
 }
